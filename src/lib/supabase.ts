afee4f1e1a7d1f42cbff1e91c593dba4b3ed5837
cuongnguyenitca <cuongnguyenitca@gmail.com>
2026-03-12 10:41:11 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://kccdttwbjheadqnakqje.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'YOUR_ANON_KEY_HERE';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjY2R0dHdiamhlYWRxbmFrcWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxODUzMzEsImV4cCI6MjA4ODc2MTMzMX0.r3UC0iwNgS6LJ3BneQwT4hXncNAuP9MZzUwJXEoCwPo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
